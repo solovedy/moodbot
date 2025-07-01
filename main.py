4a1610a5442b58d1c91bc9fdb81661f7e92484db
solovedy <diana2020.dn@gmail.com>
2025-07-01 22:44:28 +0500

    main.py

--- main.py
+++ main.py
@@ -133,3 +133,20 @@ if __name__ == "__main__":
     app.add_handler(CallbackQueryHandler(mood_callback))
     
     app.run_polling()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    keep_alive()
+bot.polling()
